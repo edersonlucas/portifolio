60739722a519f6d401e31e579f7d8441f3f1867d
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-14 00:59:33 -0300

    feat: Agora a pagina project busca as informações direto da api graphql

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import Container from './styles';
-import imageCoffe from '../../images/projects-images/coffee.svg';
-import CarouselProjects from '../../components/CarouselProjects';
+import React, { useState, useEffect } from 'react';
+import { Container, ContainerProjects } from './styles';
+import ProjectCard from '../../components/ProjectCard'
+import { gql, useQuery } from '@apollo/client';
+import Loading from '../../components/Loading';
 
-class Projects extends React.Component {
-  render() {
+const GET_PROJECT_QUERY = gql`
+  query {
+    projects {
+      name
+      description
+      alt
+      image {
+        url(transformation: {document: {output: {format: jpg}}})
+      }
+      linkRepo
+    }
+  }
+`
+
+function Projects() {
+
+  const { loading, data } = useQuery(GET_PROJECT_QUERY);
+
+  const [ revealElements, setRevealElements ] = useState(false)
+  const [ elementNeon, setElementNeon ] = useState(false)
+
+  const animatedElement = () => {
+    setTimeout(() => {
+      setRevealElements(true)
+    }, 100)
+    setTimeout(() => {
+      setElementNeon(true)
+    }, 1100)
+  }
+
+  useEffect(() => {
+    if(!loading) {
+      animatedElement();
+      window.scrollTo(0, 0);
+    }
+  })
+
+    if(loading) return <Loading/>
     return (
-      <Container>
-        <CarouselProjects />
-        <img alt="ilustração de um copo de café" src={ imageCoffe } />
+      <Container neon = { elementNeon } revel={ revealElements }>
+        <ContainerProjects revel={ revealElements }>
+        {data?.projects.map((project) => <ProjectCard key={ project.name } project={ project }/>)}
+        </ContainerProjects>
       </Container>
     )
-  }
 }
 
 export default Projects;
--- src/pages/Projects/styles.js
+++ src/pages/Projects/styles.js
@@ -1,30 +1,58 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   color: white;
-  max-width: 1200px;
-  width: 95%;
-  min-height: calc(100vh - 170px);
   overflow: hidden;
-  margin: 120px auto 50px;
+  max-width: 1200px;
+  width: 80%;
+  height: calc(100vh - 105px);
+  margin: auto;
 
-  > img {
-    max-width: 300px;
-    width: 100%;
-    filter: drop-shadow( 3px 3px 7px rgba(195, 58, 252));
-    margin-left: 50px;
-  }
-
-  @media screen and (max-width: 1150px) {
+  @media screen and (max-width: 1250px) {
     flex-direction: column;
-
-    > img {
-      margin: 20px 0;
-    }
   }
 `
 
-export default Container
+const ContainerProjects = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  transition: all 1s;
+  transform: translateX(-190%);
+  overflow: hidden;
+  overflow-y: auto;
+  gap: 20px;
+  padding: 20px;
+  height: 100%;
+
+  &::-webkit-scrollbar {
+      width: 5px;
+      border-radius: 5px;
+  };
+
+  &::-webkit-scrollbar-track {
+    background: rgb(48, 48, 50);
+    border-radius: 5px;
+  };
+
+  &::-webkit-scrollbar-thumb {
+    background: rgba(195, 58, 252);
+    border-radius: 5px;
+  };
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgba(206, 110, 246);
+  };
+
+  ${({ revel }) => revel && css`
+    transform: translateX(0%);
+  `}
+
+`
+
+export { Container, ContainerProjects }
