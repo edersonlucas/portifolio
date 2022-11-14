b75a03102b1e1450c8104459a63fd359b8150268
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-14 00:37:52 -0300

    feat: Cria o footer da pagina

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './style'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <Container>
+        <p>Desenvolvido com ❤ por: Ederson Lucas</p>
+      </Container>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/components/Footer/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 35px;
+  background: rgb(48, 48, 50, 0.9);
+  color: white;
+  z-index: 9;
+  backdrop-filter: blur(4px);
+`
+
+export { Container }
